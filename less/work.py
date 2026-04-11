5f0e79372adf90cbd872b60000290a000cd522be
Ann_kupriyanova <fedina.anna18@bk.ru>
2026-04-11 10:39:27 +0300

    new work

--- less/work.py
+++ less/work.py
@@ -1,10 +1,5 @@
-import pandas as pd
-import random as rd
-dict = {'age': [rd.randint(20, 50) for i in range(100)],
-        'user_id': [rd.randint(10000, 20000) for i in range(100)]}
-df = pd.DataFrame(dict)
-#print(df.head(10))
-print(df['age'].mean())
-df_new = df.where((df['age'] >= 20) & (df['age'] <=30))
-print(df_new['age'].mean())
-print(df.describe())
+a = [1, 5, 87, 31, 95, 4, 37, 5, 7]
+a_new = [i * 1.2 for i in a if i >= 30]
+print(a_new)
+
+b = 0
